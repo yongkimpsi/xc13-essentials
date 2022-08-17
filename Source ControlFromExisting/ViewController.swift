997b3bc620671869941d25c302480b56e6f35536
Yong Kyme <yong@psiwebware.com>
2022-08-17 17:03:55 -0400

    fixed bug a

--- Source ControlFromExisting/ViewController.swift
+++ Source ControlFromExisting/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World")
-        print("Hello World updated")
+        print("Hello World updated bug a!")
         // Do any additional setup after loading the view.
     }
 
